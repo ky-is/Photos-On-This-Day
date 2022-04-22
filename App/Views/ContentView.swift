68f0a4f705a97e80c175e29181fadd36349fdcc9
Kyle <kylecoburn@gmail.com>
2022-04-21 22:45:44 -0700

    ♻️ PermissionsView

---
+++ App/Views/ContentView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ContentView: View {
+	var body: some View {
+		ScrollView {
+			PermissionsView()
+		}
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
+
