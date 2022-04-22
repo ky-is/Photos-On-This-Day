3e1b0827b206a508e07a414e0ee9bea48b0e2813
Kyle <kylecoburn@gmail.com>
2022-04-22 13:10:24 -0700

    🐛 only filter screenshots
    Naive method filtered .none case as well

--- Shared/Photos+Extensions.swift
+++ Shared/Photos+Extensions.swift
@@ -5,7 +5,7 @@ extension PHAsset {
 		let (startDate, endDate) = Calendar.current.date(byAdding: .year, value: -yearsBack, to: date)!.getStartAndEndOfDay()
 		let fetchPhotosOptions = PHFetchOptions()
 		let creationPredicate: NSPredicate = \PHAsset.creationDate > startDate && \PHAsset.creationDate < endDate
-		let sourceTypePredicate = NSPredicate(format: "(%K & %d) == 0", #keyPath(PHAsset.mediaSubtypes), PHAssetMediaSubtype.photoScreenshot.rawValue)
+		let sourceTypePredicate = NSPredicate(format: "NOT ((%K & %d) != 0)", #keyPath(PHAsset.mediaSubtypes), PHAssetMediaSubtype.photoScreenshot.rawValue)
 		fetchPhotosOptions.predicate = CompoundPredicate<PHAsset>(type: .and, subpredicates: [creationPredicate, sourceTypePredicate])
 		fetchPhotosOptions.sortDescriptors = [NSSortDescriptor(key: #keyPath(PHAsset.creationDate), ascending: true)]
 		fetchPhotosOptions.includeAssetSourceTypes = areDuplicatesAcceptable ? [.typeCloudShared, .typeUserLibrary, .typeiTunesSynced] : [.typeUserLibrary, .typeiTunesSynced]
