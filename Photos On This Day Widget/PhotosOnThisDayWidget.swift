26cff7de71445ba69ddda16ffc0a0adb3dab3158
Kyle <kylecoburn@gmail.com>
2022-04-23 10:59:56 -0700

    🐛 cache images to reduce memory

--- Photos On This Day Widget/PhotosOnThisDayWidget.swift
+++ Photos On This Day Widget/PhotosOnThisDayWidget.swift
@@ -3,50 +3,91 @@ import SwiftUI
 import WidgetKit
 
 struct Provider: IntentTimelineProvider {
-	private func getImage(asset: PHAsset, size: CGSize, resultHandler: @escaping (UIImage?, [AnyHashable : Any]?) -> Void) {
-		PHImageManager.default().requestImage(for: asset, size: size, isSynchronous: true, highQuality: true, resultHandler: resultHandler)
+	private func getImageURL(cacheURL: URL, asset: PHAsset, size: CGSize, resultHandler: @escaping (URL) -> Void) {
+		PHImageManager.default().requestImage(for: asset, size: size, isSynchronous: true, highQuality: true) { image, _ in
+			autoreleasepool {
+				if let url = saveImageToCache(cacheURL: cacheURL, asset: asset, image: image) {
+					resultHandler(url)
+				}
+			}
+		}
 	}
 
 	func placeholder(in context: Context) -> PhotosOnThisDayEntry {
-		return PhotosOnThisDayEntry(timelineDate: Date(), photoDate: nil, score: nil, image: nil, configuration: ConfigurationIntent())
+		return PhotosOnThisDayEntry(timelineDate: Date(), photoDate: nil, score: nil, imageURL: nil, configuration: ConfigurationIntent())
 	}
 
 	func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (PhotosOnThisDayEntry) -> ()) {
 		let currentDate = Date()
 		let photosFetch = PhotosManager.shared.getPhotos(from: currentDate, yearsBack: 1, maxCount: 1)
+		var entry: PhotosOnThisDayEntry?
 		if let (score, asset) = photosFetch.first {
-			getImage(asset: asset, size: context.displaySize) { image, _ in
-				let entry = PhotosOnThisDayEntry(timelineDate: currentDate, photoDate: asset.creationDate, score: score, image: image, configuration: configuration)
-				completion(entry)
+			let cacheURL = clearCacheDirectory(for: currentDate)
+			getImageURL(cacheURL: cacheURL, asset: asset, size: context.displaySize) { url in
+				entry = PhotosOnThisDayEntry(timelineDate: currentDate, photoDate: asset.creationDate, score: score, imageURL: url, configuration: configuration)
 			}
-		} else {
-			let entry = PhotosOnThisDayEntry(timelineDate: Date(), photoDate: nil, score: nil, image: nil, configuration: configuration)
-			completion(entry)
 		}
+		completion(entry ?? PhotosOnThisDayEntry(timelineDate: Date(), photoDate: nil, score: nil, imageURL: nil, configuration: configuration))
+	}
+
+	private let cacheContainerURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("widget")
+
+	private func clearCacheDirectory(for date: Date) -> URL {
+		let datePath = DateFormatter.monthDay.string(from: date)
+		let cacheURL = cacheContainerURL.appendingPathComponent(datePath)
+		let fileManager = FileManager.default
+		do {
+			try fileManager.createDirectory(at: cacheURL, withIntermediateDirectories: true)
+			let children = try fileManager.contentsOfDirectory(atPath: cacheContainerURL.path)
+			for child in children {
+				if child != datePath {
+					try fileManager.removeItem(at: cacheContainerURL.appendingPathComponent(child))
+				}
+			}
+		} catch {
+			print(#function, error)
+		}
+		return cacheURL
+	}
+
+	private func saveImageToCache(cacheURL: URL, asset: PHAsset, image: UIImage?) -> URL? {
+		let id = String(asset.localIdentifier.split(separator: "/")[0])
+		let imageURL = cacheURL.appendingPathComponent(id)
+		if let data = image?.jpegData(compressionQuality: 1) {
+			do {
+				try data.write(to: imageURL)
+				return imageURL
+			} catch {
+				print(error)
+			}
+		}
+		return nil
 	}
 
 	func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+		let currentDate = Date()
+		let cacheURL = clearCacheDirectory(for: currentDate)
 		let manager = PhotosManager()
 
 		let calendar = Calendar.current
-		let currentDate = Date()
 		let nextDayStart = calendar.startOfDay(for: calendar.date(byAdding: .day, value: 1, to: currentDate)!)
 		let timeForUpdates = nextDayStart.timeIntervalSince(currentDate)
-		let maxEntries = Int((timeForUpdates / (15 * .minute)).rounded(.down))
+		let minutesPerUpdate: Double = context.family == .systemExtraLarge ? 60 : 30 //TODO improve memory handling
+		let maxEntries = Int((timeForUpdates / (minutesPerUpdate * .minute)).rounded(.down))
 		var entries: [PhotosOnThisDayEntry] = []
 		let photosFetch = manager.getPhotos(from: Date(), yearsBack: 16, maxCount: maxEntries).shuffled()
 		let timePerUpdate = timeForUpdates / Double(photosFetch.count)
-		var pendingRequests = 0
 		for (offset, scoreAsset) in photosFetch.enumerated() {
-			let entryDate = currentDate.addingTimeInterval(timePerUpdate * Double(offset))
-			pendingRequests += 1
-			getImage(asset: scoreAsset.asset, size: context.displaySize) { image, _ in
-				let entry = PhotosOnThisDayEntry(timelineDate: entryDate, photoDate: scoreAsset.asset.creationDate, score: scoreAsset.score, image: image, configuration: configuration)
-				entries.append(entry)
+			autoreleasepool {
+				let entryDate = currentDate.addingTimeInterval(timePerUpdate * Double(offset))
+				getImageURL(cacheURL: cacheURL, asset: scoreAsset.asset, size: context.displaySize) { url in
+					let entry = PhotosOnThisDayEntry(timelineDate: entryDate, photoDate: scoreAsset.asset.creationDate, score: scoreAsset.score, imageURL: url, configuration: configuration)
+					entries.append(entry)
+				}
 			}
 		}
 		if entries.isEmpty {
-			entries.append(PhotosOnThisDayEntry(timelineDate: currentDate, photoDate: nil, score: nil, image: nil, configuration: configuration))
+			entries.append(PhotosOnThisDayEntry(timelineDate: currentDate, photoDate: nil, score: nil, imageURL: nil, configuration: configuration))
 		}
 		let timeline = Timeline(entries: entries, policy: .after(nextDayStart))
 		completion(timeline)
@@ -56,14 +97,14 @@ struct Provider: IntentTimelineProvider {
 struct PhotosOnThisDayEntry: TimelineEntry {
 	let date: Date
 	let photoDate: Date?
-	let image: UIImage?
+	let imageURL: URL?
 	var relevance: TimelineEntryRelevance?
 	let configuration: ConfigurationIntent
 
-	init(timelineDate: Date, photoDate: Date?, score: Float?, image: UIImage?, configuration: ConfigurationIntent) {
+	init(timelineDate: Date, photoDate: Date?, score: Float?, imageURL: URL?, configuration: ConfigurationIntent) {
 		self.date = timelineDate
 		self.photoDate = photoDate
-		self.image = image
+		self.imageURL = imageURL
 		self.relevance = .init(score: score ?? 0)
 		self.configuration = configuration
 	}
@@ -71,10 +112,8 @@ struct PhotosOnThisDayEntry: TimelineEntry {
 
 @main
 struct PhotosOnThisDayWidget: Widget {
-	let kind: String = WidgetKind
-
 	var body: some WidgetConfiguration {
-		IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+		IntentConfiguration(kind: WidgetKind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
 			PhotosOnThisDayWidgetEntryView(entry: entry)
 		}
 			.configurationDisplayName("On This Day")
@@ -85,21 +124,26 @@ struct PhotosOnThisDayWidget: Widget {
 
 // View
 
-struct PhotosOnThisDayWidgetEntryView : View {
+struct PhotoWidgetView: View {
 	var entry: Provider.Entry
-
-	@Environment(\.widgetFamily) private var widgetFamily
+	var size: CGSize
 
 	var body: some View {
-		GeometryReader { geometry in
-			ZStack {
-				Group {
-					if let image = entry.image {
-						Image(uiImage: image)
-							.resizable()
-							.aspectRatio(contentMode: .fill)
-							.frame(width: geometry.size.width, height: geometry.size.height)
-					} else {
+		Group {
+			if let imageURL = entry.imageURL, let image = UIImage(contentsOfFile: imageURL.path) {
+				Image(uiImage: image)
+					.resizable()
+					.aspectRatio(contentMode: .fill)
+					.frame(width: size.width, height: size.height)
+			} else {
+				if entry.photoDate == nil {
+					Image(systemName: "photo")
+						.font(.system(size: size.height * 0.75))
+						.foregroundColor(.primary.opacity(0.06))
+						.frame(width: size.width, height: size.height)
+						.unredacted()
+				} else {
+					Group {
 						let photosAuthStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
 						if photosAuthStatus == .authorized {
 							Image(systemName: "photo")
@@ -110,26 +154,39 @@ struct PhotosOnThisDayWidgetEntryView : View {
 						}
 					}
 				}
+			}
+		}
+	}
+}
+
+struct PhotosOnThisDayWidgetEntryView: View {
+	var entry: Provider.Entry
+
+	@Environment(\.widgetFamily) private var widgetFamily
+
+	var body: some View {
+		GeometryReader { geometry in
+			ZStack {
+				PhotoWidgetView(entry: entry, size: geometry.size)
 					.overlay(alignment: .bottomTrailing) {
 						VStack(alignment: .trailing) {
-							if entry.image != nil {
+							if entry.imageURL != nil || entry.photoDate == nil {
 								Group {
 									let isSmall = widgetFamily == .systemSmall
 									let isLarge = !isSmall && widgetFamily != .systemMedium
+									let displayDate = entry.photoDate ?? entry.date
 									if !isSmall {
 										Group {
 											if isLarge {
-												Text(entry.photoDate ?? entry.date, style: .date)
+												Text(displayDate, style: .date)
 											} else {
-												Text(entry.photoDate ?? entry.date, formatter: DateFormatter.monthDay)
+												Text(displayDate, formatter: DateFormatter.monthDay)
 											}
 										}
 											.font(.system(isLarge ? .title : .title2, design: .rounded).weight(.semibold))
 									}
-									if let date = entry.photoDate {
-										Text(date, format: .relative(presentation: .numeric, unitsStyle: .wide))
-											.font(.system(.callout, design: .rounded).weight(.semibold))
-									}
+									Text(displayDate, format: .relative(presentation: .numeric, unitsStyle: .wide))
+										.font(.system(.callout, design: .rounded).weight(.semibold))
 								}
 									.foregroundColor(.white)
 									.shadow(color: .black, radius: 1.5, x: 0, y: 1)
@@ -163,7 +220,7 @@ struct PhotosOnThisDayWidget_Previews: PreviewProvider {
 	static let photosFetch = PhotosManager.shared.getPhotos(from: Date(), yearsBack: 1, maxCount: 1).first
 
 	static var previews: some View {
-		PhotosOnThisDayWidgetEntryView(entry: PhotosOnThisDayEntry(timelineDate: Date(), photoDate: nil, score: photosFetch?.score, image: nil, configuration: ConfigurationIntent()))
+		PhotosOnThisDayWidgetEntryView(entry: PhotosOnThisDayEntry(timelineDate: Date(), photoDate: nil, score: photosFetch?.score, imageURL: nil, configuration: ConfigurationIntent()))
 			.previewContext(WidgetPreviewContext(family: .systemSmall))
 	}
 }
