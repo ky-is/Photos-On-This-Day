b9fc7615d8f6069035968ac4fd6f96d6452a08c0
Kyle <kylecoburn@gmail.com>
2022-04-25 13:37:53 -0700

    💬 Smart Stacks

--- App/Views/HelpAddWidgetView.swift
+++ App/Views/HelpAddWidgetView.swift
@@ -12,9 +12,9 @@ struct HelpAddWidgetView: View {
 			VStack(alignment: .leading, spacing: 16) {
 				Text("**1.** Long press in an empty spot on your Home Screen.")
 				Text("**2.** Tap the \"+\" button in the top-left.")
-				Text("**3.** Search for \"On This Day\" (it's been copied to your clipboard), or find it in the list.")
+				Text("**3.** Search for \"On This Day\" (it's been copied to your clipboard), or find it from the list.")
 				Text("**4.** Swipe left to choose a size, then press the \"Add Widget\" button at the bottom.")//You can configure the Widget by tapping it while edit mode is active!")
-				Text("Tip: If your photo library is sparse, try dragging the Widget into a Stack. For days it finds a photo, the _On This Day_ Widget can automatically show at the top!")
+				Text("Tip: If your photo library is sparse, the _On This Day_ Widget works best in a Smart Stack. Drag a system Photos Widget (with the same size) on top of this Widget to create a new Smart Stack. This lets _On This Day_ show at the top for days with photos, and otherwise the default Photos Widget will be shown as a fallback.")
 					.italic()
 					.padding()
 					.multilineTextAlignment(.center)
