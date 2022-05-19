2870f3f43dc49a833d1ddbeca7ad20f0d235891f
Kyle <kylecoburn@gmail.com>
2022-05-19 08:31:27 -0700

    💄 put hide button at bottom of tutorial

--- App/Views/HelpAddWidgetView.swift
+++ App/Views/HelpAddWidgetView.swift
@@ -18,9 +18,9 @@ struct HelpAddWidgetView: View {
 		self.inSheet = inSheet
 		let item = AVPlayerItem(url: URL(string: "https://kcdn.netlify.app/widget.mov")!)
 		player = AVQueuePlayer(playerItem: item)
+		player.isMuted = true
 		player.externalPlaybackVideoGravity = .resizeAspect
 		player.audiovisualBackgroundPlaybackPolicy = .pauses
-		player.isMuted = true
 		looper = AVPlayerLooper(player: player, templateItem: item)
 	}
 
@@ -55,10 +55,16 @@ struct HelpAddWidgetView: View {
 				Text("**2.** Tap the \"+\" button in the top-left.")
 				Text("**3.** Search for \"On This Day\" (it's been copied to your clipboard), or find it from the list.")
 				Text("**4.** Swipe left to choose a size, then press the \"Add Widget\" button at the bottom.")//You can configure the Widget by tapping it while edit mode is active!")
-				Text("Tip: If your photo library is sparse, the _On This Day_ Widget works best in a Smart Stack. Drag a system Photos Widget (with the same size) on top of this Widget to create a new Smart Stack. This lets _On This Day_ show at the top for days with photos, and otherwise the default Photos Widget will be shown as a fallback.")
+				Text("Tip: If your photo library is sparse, the _On This Day_ Widget works best in a Smart Stack.\n\nDrag a system Photos Widget (with the same size) on top of this Widget to create a new Smart Stack. This lets _On This Day_ show at the top for days with photos, and otherwise the default Photos Widget will be shown as a fallback.")
 					.italic()
-					.padding()
-					.multilineTextAlignment(.center)
+				if inSheet {
+					Button("Don't show again") {
+						UserDefaults.standard.set(true, forKey: UserDefaults.Key.dismissedAddWidget)
+						dismiss()
+					}
+						.buttonStyle(.bordered)
+						.frame(maxWidth: .greatestFiniteMagnitude)
+				}
 			}
 				.onAppear {
 					UIPasteboard.general.string = "On This Day"
@@ -68,14 +74,6 @@ struct HelpAddWidgetView: View {
 			.navigationTitle("Add a Widget")
 			.navigationBarTitleDisplayMode(.inline)
 			.toolbar {
-				ToolbarItem(placement: .destructiveAction) {
-					if inSheet {
-						ToolbarButton("Don't show again") {
-							UserDefaults.standard.set(true, forKey: UserDefaults.Key.dismissedAddWidget)
-							dismiss()
-						}
-					}
-				}
 				ToolbarItem(placement: .cancellationAction) {
 					if inSheet {
 						ToolbarButton("Close") {
