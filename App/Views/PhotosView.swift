6288010dbad34ad6e95bf9ee4126cbf300022b50
Kyle <kylecoburn@gmail.com>
2022-05-26 11:09:46 -0700

    ✨ show current year photos on empty days

--- App/Views/PhotosView.swift
+++ App/Views/PhotosView.swift
@@ -9,33 +9,49 @@ struct PhotosView: View {
 		PhotosViewContent(date: date)
 	}
 }
+
 struct PhotosViewContent: View {
 	let fetches: [PhotosFetch]
+	let currentYearFetch: PhotosFetch?
+	let hasNoPastPhotos: Bool
+	let dateID: String
+	let hiddenPhotosCount: Int
 
 	init(date: Date) {
 		self.fetches = (1...MaxYearsBack).map { PhotosFetch(fromDate: date, yearsBack: $0) }
+		self.hasNoPastPhotos = fetches.allSatisfy({ $0.assets.isEmpty })
+		self.dateID = fetches.first?.dateID ?? ""
+		self.currentYearFetch = self.hasNoPastPhotos ? PhotosFetch(fromDate: date, yearsBack: 0) : nil
+		self.hiddenPhotosCount = SyncStorage.shared.filterPhotos[dateID]?.count ?? 0
 	}
 
 	var body: some View {
 		VStack {
-			if fetches.allSatisfy({ $0.assets.isEmpty }) {
+			if hasNoPastPhotos {
 				VStack {
 					Group {
 						Text("No photos from past years on this day")
 							.font(.system(.title3, design: .rounded).weight(.medium))
 							.padding(.vertical)
+							.foregroundColor(.secondary)
 						if !SyncStorage.shared.filterShowShared {
 							Text("Try enabling iCloud Shared photos from the filter button at the top to include photos from friends and family!")
+								.foregroundColor(.secondary)
 						} else {
 							Text("You can browse other days\nusing the ◀︎ and ▶︎ buttons")
+								.foregroundColor(.secondary)
 						}
 					}
 						.fixedSize(horizontal: false, vertical: true)
 				}
-					.foregroundColor(.secondary)
 					.multilineTextAlignment(.center)
 					.frame(maxWidth: 512)
 					.padding()
+				if let currentYearFetch = currentYearFetch {
+					LazyVStack(alignment: .leading, spacing: 0, pinnedViews: [.sectionHeaders]) {
+						PhotosYearView(fetch: currentYearFetch)
+					}
+				}
 			} else {
 				LazyVStack(alignment: .leading, spacing: 0, pinnedViews: [.sectionHeaders]) {
 					ForEach(fetches) { fetch in
@@ -43,9 +59,9 @@ struct PhotosViewContent: View {
 					}
 				}
 			}
-			if let fetch = fetches.first, let hiddenPhotosCount = SyncStorage.shared.filterPhotos[fetch.dateID]?.count, hiddenPhotosCount > 0 {
+			if hiddenPhotosCount > 0 {
 				Button {
-					SyncStorage.shared.filterPhotos[fetch.dateID] = nil
+					SyncStorage.shared.filterPhotos[dateID] = nil
 				} label: {
 					Text("Restore \("hidden photo".pluralize(hiddenPhotosCount))")
 						.font(.system(.title3, design: .rounded))
